1e6a4fa59cc8df7caf6f62d79875a4a1f521d5ad
Mohammed Amin Ibrahim <mohammedaminibrahim10@gmail.com>
2022-07-28 14:59:00 +0000

    Added backend with API basic

---
+++ data.js
@@ -0,0 +1,93 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'shirt',
+            image: './images/p2.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+        {
+            name: 'Adidas Fit Pant',
+            slug: 'Adidas-slim',
+            category: 'shirt',
+            image: './images/p2.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+        {
+            name: 'Toronto Big Shirt',
+            slug: 'toronot-slim-shirt',
+            category: 'shirt',
+            image: './images/p2.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+        {
+            name: 'Adidas Fit Pant',
+            slug: '-slim-shirt',
+            category: 'shirt',
+            image: './images/p2.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+        {
+            name: 'Toronton Big Shirt',
+            slug: 'toront-slim-shirt',
+            category: 'shirt',
+            image: './images/p2.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+        {
+            name: 'ndk Fit Pant',
+            slug: 'Adidas-slim-shirt',
+            category: 'shirt',
+            image: './images/p2.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+        {
+            name: 'Newo Big Shirt',
+            slug: 'tornot-slim-shirt',
+            category: 'shirt',
+            image: './images/p2.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        }
+    ]
+    
+    
+}
+
+
+export default data;
